49b798827c7a290db6eefa666bc3e94b763f2b55
leirdan <uouleirdan@gmail.com>
2022-11-01 20:30:08 -0300

    Linkando a pasta 'public' fazendo funcionar o CSS

--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const server = express();
+const path = require("path");
+
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+server.use(express.static(path.join(__dirname, "public")));
+
+server.get("/inicio", (req, res) => {
+  res.sendFile(__dirname + "/html/index.html");
+});
 
 server.listen(3333, () => {
   console.log("Servidor aberto!");
